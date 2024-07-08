83ea6cd8209c33100312d7baa97172f513ff2c65
Dell <Dell@Megha>
2024-07-08 17:27:17 +0530

    First commit

---
+++ Hello/src/Helloa/Helloa.java
@@ -0,0 +1,12 @@
+package Helloa;
+
+public class Helloa {
+    public void sayHello() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        Helloa hello = new Helloa();
+        hello.sayHello();
+    }
+}
